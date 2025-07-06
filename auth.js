a076e0c99cd12724b56ff3e53e608b73ffaa412e
Your Name <youremail@example.com>
2025-07-06 08:10:30 +0300

    Initial commit  asnan360 project

---
+++ auth.js
@@ -0,0 +1,39 @@
+// auth.js
+const registerForm = document.getElementById('registerForm');
+const loginForm = document.getElementById('loginForm');
+
+if (registerForm) {
+  registerForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    const email = registerForm.email.value.trim();
+    const password = registerForm.password.value;
+
+    const res = await fetch('/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    });
+
+    const msg = await res.text();
+    alert(msg);
+    if (msg.includes('✅')) window.location.href = "login.html";
+  });
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    const email = loginForm.email.value.trim();
+    const password = loginForm.password.value;
+
+    const res = await fetch('/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    });
+
+    const msg = await res.text();
+    alert(msg);
+    if (msg.includes('✅')) window.location.href = "index.html";
+  });
+}
